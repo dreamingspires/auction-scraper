acb19bb33e7b3cec393e6f84810bf3b09a1d024d
Edd Salkield <edd@salkield.uk>
2020-08-21 01:01:27 +0100

    Remove test code

--- profile_test.py
+++
@@ -1,3 +0,0 @@
-from auction_scraper.scrapers.liveauctioneers.scraper import LiveAuctioneersAuctionScraper
-s = LiveAuctioneersAuctionScraper('./app.db', './data')
-auction = s.scrape_profile_to_db('https://www.liveauctioneers.com/auctioneer/197/hindman/')
--- search_test.py
+++
@@ -1,3 +0,0 @@
-from auction_scraper.scrapers.liveauctioneers.scraper import LiveAuctioneersAuctionScraper
-s = LiveAuctioneersAuctionScraper('./app.db', './data')
-auction = s.scrape_search_to_db('art', 1)
